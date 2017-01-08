62fbbcadfb3d5ad65eb95db7c4674522ed354cf7
Edward Kmett <ekmett@gmail.com>
2017-01-08 09:56:49 -0500

    try for more robust __has_attribute detection on clang

--- attribute.h
+++ attribute.h
@@ -1,25 +1,55 @@
 #pragma once
 
+#ifndef __has_declspec_attribute
 #ifdef _MSC_VER
-#define FIB_DECLSPEC_NOALIAS __declspec(noalias)
-#define FIB_DECLSPEC_RESTRICT __declspec(restrict)
+#define __has_declspec_attribute(X) 1
 #else
-#define FIB_DECLSPEC_NOALIAS
-#define FIB_DECLSPEC_RESTRICT
+#define __has_declspec_attribute(X) 0
+#endif
+#endif
+
+#ifndef __has_attribute
+#define __has_attribute(X) 0
 #endif
 
 #ifdef GCC
-#define FIB_ATTRIBUTE_ALLOC_ALIGN(N) __attribute__((alloc_align(N)))
-#define FIB_ATTRIBUTE_ALLOC_SIZE(N) __attribute__((alloc_size(N)))
+#define __has_gcc_attribute(X) 1
+#else
+#define __has_gcc_attribute(X) __has_attribute(X)
+#endif
+
+#if __has_declspec_attribute(noalias)
+#define FIB_DECLSPEC_NOALIAS __declspec(noalias)
+#else
+#define FIB_DECLSPEC_NOALIAS
+#endif
+
+#if __has_declspec_attribute(restrict)
+#define FIB_DECLSPEC_RESTRICT __declspec(restrict)
+#else
+#define FIB_DECLSPEC_RESTRICT
+#endif
+
+#if __has_gcc_attribute(malloc)
 #define FIB_ATTRIBUTE_MALLOC __attribute((malloc))
 #else
-#define FIB_ATTRIBUTE_ALLOC_ALIGN(N)
-#define FIB_ATTRIBUTE_ALLOC_SIZE(N)
 #define FIB_ATTRIBUTE_MALLOC
 #endif
 
-#if define(GCC) || defined(__clang__)
+#if __has_gcc_attribute(returns_nonnull)
 #define FIB_ATTRIBUTE_RETURNS_NONNULL __attribute((returns_nonnull))
 #else
 #define FIB_ATTRIBUTE_RETURNS_NONNULL
+#endif
+
+#if __has_gcc_attribute(alloc_align)
+#define FIB_ATTRIBUTE_ALLOC_ALIGN(N) __attribute__((alloc_align(N))
+#else
+#define FIB_ATTRIBUTE_ALLOC_ALIGN(N)
+#endif
+
+#if __has_gcc_attribute(alloc_size)
+#define FIB_ATTRIBUTE_ALLOC_SIZE(N) __attribute__((alloc_size(N))
+#else
+#define FIB_ATTRIBUTE_ALLOC_SIZE(N)
 #endif
