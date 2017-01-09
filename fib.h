c2ddc7d4d7ee4dee8d134adecc5cc64f0b948742
Edward Kmett <ekmett@gmail.com>
2017-01-08 23:47:16 -0500

    added LICENSE file

--- fib.h
+++ fib.h
@@ -2,15 +2,11 @@
 
 #include "fib/attribute.h"
 #include "fib/chrono.h"
+#include "fib/memory.h"
 #include "fib/worker.h"
 
 /// @file fib.h
 /// @brief @ref fib
-
-/// @package fib
+/// @author Edward A. Kmett
+/// @copyright (C) 2017
 /// @brief a library for light-weight fibers and polling io
-
-/// @namespace fib
-/// @brief light-weight fibers and work-stealing
-
-
